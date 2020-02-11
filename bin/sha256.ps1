6940323d21e30dbfdfad653047c07b0f4e5cf0d9
zombie110year <zombie110year@outlook.com>
2020-02-11 15:36:14 +0800

    new(bin): sha256 script

---
+++ bin/sha256.ps1
@@ -0,0 +1,12 @@
+<# 获取文件的 SHA256 Hash #>
+param (
+  [string]$filepath
+)
+$oldfile = (Get-Item $filepath)
+$newpath = "./tmp/$($oldfile.Name)"
+cp -Force $oldfile $newpath
+$file = (Get-Item $newpath)
+$hash = (Get-FileHash -Algorithm SHA256 $file).Hash
+Set-Content -Encoding ASCII -Path "./tmp/$($file.Name).sha256" -Value "$hash"
+echo $file.Name
+echo "sha256:$hash"
